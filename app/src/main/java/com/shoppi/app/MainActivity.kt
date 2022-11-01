f76a3c9e56f0323c22db8645745e2de6ad09e07f
voemvnt <voemvnt@gmail.com>
2022-11-02 01:36:30 +0900

    5강까지 완료

--- app/src/main/java/com/shoppi/app/MainActivity.kt
+++ app/src/main/java/com/shoppi/app/MainActivity.kt
@@ -2,10 +2,15 @@ package com.shoppi.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_detail)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
     }
+
 }
